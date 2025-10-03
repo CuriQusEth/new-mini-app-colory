189af94ddaf90ad04abb94717ea7f9a30ad1794d
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-03 23:53:41 +0300

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,8 +1,6 @@
 import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
-  "baseBuilder": {
-    "allowedAddresses": ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
-  }
+
 export async function GET() {
   return Response.json(withValidManifest(minikitConfig));
 }
