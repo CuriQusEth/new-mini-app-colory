ffa869626c9bbb553b0861fde5309e5ea44e8b59
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-04 20:34:03 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -11,9 +11,7 @@ export const minikitConfig = {
       'eyJkb21haW4iOiJuZXctbWluaS1hcHAtY29sb3J5LnZlcmNlbC5hcHAifQ',
     signature:
       'MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi',
-  "baseBuilder": {
-    "allowedAddresses": ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
-  }
+  },
   miniapp: {
     version: '1',
     name: 'COLORY',
