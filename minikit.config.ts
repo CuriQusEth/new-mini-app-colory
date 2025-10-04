802cab05bdc4744d197fa2084a2774c707d73e7e
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-04 20:35:13 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,34 +1,26 @@
-/**
- * MiniApp configuration object. Must follow the Farcaster MiniApp specification.
- *
- * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
- */
-export const minikitConfig = {
-  accountAssociation: {
-    header:
-      'eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ',
-    payload:
-      'eyJkb21haW4iOiJuZXctbWluaS1hcHAtY29sb3J5LnZlcmNlbC5hcHAifQ',
-    signature:
-      'MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi',
+{
+  "accountAssociation": {
+    "header": "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
+    "payload": "eyJkb21haW4iOiJuZXctbWluaS1hcHAtY29sb3J5LnZlcmNlbC5hcHAifQ",
+    "signature": "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
   },
-  miniapp: {
-    version: '1',
-    name: 'COLORY',
-    subtitle: 'Your AI Ad Companion',
-    description: 'Ads',
-    screenshotUrls: ['https://base-app-colory.vercel.app/screenshot-portrait.png'],
-    iconUrl: 'https://base-app-colory.vercel.app/blue-icon.png',
-    splashImageUrl: 'https://base-app-colory.vercel.app/blue-hero.png',
-    splashBackgroundColor: '#000000',
-    homeUrl: 'https://base-app-colory.vercel.app/',
-    webhookUrl: 'https://base-app-colory.vercel.app/api/webhook',
-    primaryCategory: 'social',
-    tags: ['marketing', 'ads', 'quickstart', 'waitlist'],
-    heroImageUrl: 'https://base-app-colory.vercel.app/blue-hero.png',
-    tagline: '',
-    ogTitle: '',
-    ogDescription: '',
-    ogImageUrl: 'https://base-app-colory.vercel.app/blue-hero.png',
+  "baseBuilder": {
+    "allowedAddresses": ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
   },
-} as const;
+  "miniapp": {
+    "version": "1",
+    "name": "COLORY",
+    "subtitle": "Your AI Ad Companion",
+    "description": "Ads",
+    "screenshotUrls": ["https://base-app-colory.vercel.app/screenshot-portrait.png"],
+    "iconUrl": "https://base-app-colory.vercel.app/blue-icon.png",
+    "splashImageUrl": "https://base-app-colory.vercel.app/blue-hero.png",
+    "splashBackgroundColor": "#000000",
+    "homeUrl": "https://base-app-colory.vercel.app/",
+    "webhookUrl": "https://base-app-colory.vercel.app/api/webhook",
+    "primaryCategory": "social",
+    "tags": ["marketing", "ads", "quickstart", "waitlist"],
+    "heroImageUrl": "https://base-app-colory.vercel.app/blue-hero.png",
+    "ogImageUrl": "https://base-app-colory.vercel.app/blue-hero.png"
+  }
+}
