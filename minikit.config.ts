1be254e04037482bc90da4f959c3a42b8c0faeaf
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-05 17:29:54 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,10 +1,10 @@
 export const minikitConfig = {
-  accountAssociation: {
-    header: "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
-    payload: "eyJkb21haW4iOiJiYXNlLWFwcC1jb2xvcnkudmVyY2VsLmFwcCJ9",
-    signature:
-      "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
-  },
+{
+  "accountAssociation": {
+    "header": "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
+    "payload": "eyJkb21haW4iOiJuZXctbWluaS1hcHAtY29sb3J5LnZlcmNlbC5hcHAifQ",
+    "signature": "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
+  }
 
   baseBuilder: {
     allowedAddresses: ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
