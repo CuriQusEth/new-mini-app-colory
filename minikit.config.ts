3da83286bbf68fe77dc7dde4d21cef8e45d58aa9
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-05 17:34:20 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,9 +1,13 @@
 export const minikitConfig = {
-{
-  "accountAssociation": {
-    "header": "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
-    "payload": "eyJkb21haW4iOiJuZXctbWluaS1hcHAtY29sb3J5LnZlcmNlbC5hcHAifQ",
-    "signature": "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
+  accountAssociation: {
+    header:
+      "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
+    payload:
+      "eyJkb21haW4iOiJiYXNlLWFwcC1jb2xvcnkudmVyY2VsLmFwcCJ9",
+    signature:
+      "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
+  },
+
   baseBuilder: {
     allowedAddresses: ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
   },
@@ -19,17 +23,18 @@ export const minikitConfig = {
     imageUrl: "https://i.imgur.com/jrMQRXC.png",
     splashImageUrl: "https://i.imgur.com/jrMQRXC.png",
     splashBackgroundColor: "#000000",
-    
-    // Ana bağlantılar
+
+    // Ana URL’ler
     homeUrl: "https://base-app-colory.vercel.app/",
     webhookUrl: "https://base-app-colory.vercel.app/",
-    
+
     primaryCategory: "games",
     tags: ["games", "memory", "puzzle", "brain"],
     heroImageUrl: "https://i.imgur.com/jrMQRXC.png",
     tagline: "Train Your Brain",
     ogTitle: "Color Memory Game",
-    ogDescription: "A simple and fun game designed to improve memory and focus.",
+    ogDescription:
+      "A simple and fun game designed to improve memory and focus.",
     ogImageUrl: "https://i.imgur.com/jrMQRXC.png"
   }
 };
