bc4a9c22fae34e9cd895174033ed5ea14240c3da
CuriQusEth <77315943+CuriQusEth@users.noreply.github.com>
2025-10-05 16:49:22 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -2,23 +2,30 @@ export const minikitConfig = {
   accountAssociation: {
     header: "eyJmaWQiOjUwMzg5MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ1YWZDNWQ1MzA4ZTM0ODJCMEU4ZEVBNTRGM2JjMTRCNTkxQzgzQzkifQ",
     payload: "eyJkb21haW4iOiJiYXNlLWFwcC1jb2xvcnkudmVyY2VsLmFwcCJ9",
-    signature: "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
+    signature:
+      "MHhhYTU1MmE2MmE3Y2Y3NjRiMzI5OWVlYTU3NGJhNGYwODM3NjRhY2VhYzM2Zjc3MTY1MmM5MmRhZmZhZmY3MjMzNDhiYThmYWU2ZDAzNWJiNzQ4ZTJlYzZmNTFlYWQ5ZDIwNGNmZTg5MzFmMmUyMDU4ODI5NTU5ZGMyODM2MDcxYzFi"
   },
+
   baseBuilder: {
     allowedAddresses: ["0x29536D0bc1004ab274c4F0F59734Ad74D4559b7B"]
   },
+
   miniapp: {
     version: "1",
     name: "Color Memory",
     subtitle: "COLORFUL",
-    description: "A simple and fun game designed to improve memory and focus. Remember and repeat color sequences that get longer as you progress.",
+    description:
+      "A simple and fun game designed to improve memory and focus. Remember and repeat color sequences that get longer as you progress.",
     screenshotUrls: ["https://i.imgur.com/jrMQRXC.png"],
     iconUrl: "https://i.imgur.com/jrMQRXC.png",
     imageUrl: "https://i.imgur.com/jrMQRXC.png",
     splashImageUrl: "https://i.imgur.com/jrMQRXC.png",
     splashBackgroundColor: "#000000",
+    
+    // Ana bağlantılar
     homeUrl: "https://base-app-colory.vercel.app/",
     webhookUrl: "https://base-app-colory.vercel.app/",
+    
     primaryCategory: "games",
     tags: ["games", "memory", "puzzle", "brain"],
     heroImageUrl: "https://i.imgur.com/jrMQRXC.png",
